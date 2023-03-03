4ed11a610e48b125d65977a430c53361f086f577
nGeorgit <113199881+nGeorgit@users.noreply.github.com>
2023-03-03 16:38:54 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
-var collums = 100;
+var collums = 100;//x
 
 imageEl = document.getElementById('image');
 
-var cur_choice = 0// 0: null, 1: wall, 2: place, 3: entry
+var cur_choice = 0// 0: null, 1: wall, 2: place, 3: stairs, 4: elevator
 
 var types = [
     {
@@ -23,7 +23,7 @@ var types = [
     },
     {
         "type": "wall",
-        "color": "black",
+        "color": "red",
         "walkable": false,
         "change": function(e) {
             e.style.backgroundColor = this.color
@@ -55,7 +55,7 @@ var types = [
         }
     },
     {
-        "type": "entry",
+        "type": "stairs",
         "color": "green",
         "walkable": true,
         "change": function(e) {
@@ -64,16 +64,33 @@ var types = [
         },
         "extra": function() {
             return {
-                entryTo: prompt("Entry to building: "),
-                num: prompt("Entry num: ")
+                id: prompt("Entry id: "),
+                level: prompt("Entry level: ")
             }
         },
         "text": function(extra) {
-            return extra.entryTo + "," + extra.num
+            return extra.id
+        }
+    },
+    {
+        "type": "elevator",
+        "color": "greenyellow",
+        "walkable": true,
+        "change": function(e) {
+            e.style.backgroundColor = this.color
+            e.style.opacity = 0.6
+        },
+        "extra": function() {
+            return {
+                id: prompt("Entry id: "),
+                level: prompt("Entry level: ")
+            }
+        },
+        "text": function(extra) {
+            return extra.id
         }
     }
 ]
-
 
 
 imgInp.onchange = evt => {
@@ -91,7 +108,7 @@ var btn = document.getElementById(
         }
 
 document.addEventListener("keydown", (event) => {
-    if (event.key<=4 && event.key>=1) {
+    if (event.key<=5 && event.key>=1) {
         cur_choice = event.key - 1
         document.getElementById('choice').innerHTML = types[cur_choice].type + "(" + event.key + ")"
     }
@@ -125,8 +142,8 @@ function idToCord(id){
 function clickNode(e)
 {
     cord = idToCord(e.id)
-    x = cord[1]
-    y = cord[0]
+    x = cord[0]
+    y = cord[1]
 
     types[cur_choice].change(e);
     nodes[x][y].type = types[cur_choice].type;
@@ -141,17 +158,17 @@ function setImage(){
 
     imageRatio = imageEl.clientHeight/image.clientWidth;
     nodeSize = imageEl.clientWidth/collums
-    rows = collums*imageRatio;
+    rows = collums*imageRatio;//y
     console.log(imageEl.clientWidth)
     console.log(nodeSize)
 
     nodes = new Array();
-    for (let i = 0; i < rows; i++) {                    //we creat a list with all the nodes
+    for (let i = 0; i < collums; i++) {                    //we creat a list with all the nodes
         nodes[i] = new Array();
-        for (let j =0; j < collums; j++) {       
+        for (let j =0; j < rows; j++) {       
             nodes[i][j] = {
-                x: j,
-                y: i,
+                x: i,
+                y: j,
                 walkable: true,
                 type: null,
                 extra: null
@@ -173,12 +190,12 @@ function setImage(){
         divRow.className = 'row'                      //add a class 
         for (let j =0; j < collums; j++) {       
             let divCollume = document.createElement("DIV")   //create the collum(div with id: 'y,x' and class: 'node')
-            divCollume.id = cordToId(nodes[i][j].x, nodes[i][j].y);                 //add id
+            divCollume.id = cordToId(nodes[j][i].x, nodes[j][i].y);                 //add id
             divCollume.className = 'node'                  //add a class
             divCollume.style.width = nodeSize + 'px';        //set size
             divCollume.style.height = nodeSize + 'px';
-            divCollume.addEventListener('mouseenter', (e) => {if(mouseDown==1 && (cur_choice==0 || cur_choice==1)) {clickNode(e.path[0])}})
-            divCollume.addEventListener('mousedown', (e) => {clickNode(e.path[0])})
+            divCollume.addEventListener('mouseenter', (e) => {if(mouseDown==1 && (cur_choice==0 || cur_choice==1)) {clickNode(e.target)}})
+            divCollume.addEventListener('mousedown', (e) => {clickNode(e.target)})
               
             divRow.appendChild(divCollume)
         }
@@ -188,9 +205,424 @@ function setImage(){
     
  }
 
+//------------------------
+
+function pathTo(node) {
+    var curr = node;
+    var path = [];
+    while (curr.parent) {
+      path.unshift(curr);
+      curr = curr.parent;
+    }
+    return path;
+  }
+  
+  function getHeap() {
+    return new BinaryHeap(function(node) {
+      return node.f;
+    });
+  }
+  
+  var astar = {
+    /**
+    * Perform an A* Search on a graph given a start and end node.
+    * @param {Graph} graph
+    * @param {GridNode} start
+    * @param {GridNode} end
+    * @param {Object} [options]
+    * @param {bool} [options.closest] Specifies whether to return the
+               path to the closest node if the target is unreachable.
+    * @param {Function} [options.heuristic] Heuristic function (see
+    *          astar.heuristics).
+    */
+    search: function(graph, start, end, options) {
+      graph.cleanDirty();
+      options = options || {};
+      var heuristic = options.heuristic || astar.heuristics.manhattan;
+      var closest = options.closest || false;
+  
+      var openHeap = getHeap();
+      var closestNode = start; // set the start node to be the closest if required
+  
+      start.h = heuristic(start, end);
+      graph.markDirty(start);
+  
+      openHeap.push(start);
+  
+      while (openHeap.size() > 0) {
+  
+        // Grab the lowest f(x) to process next.  Heap keeps this sorted for us.
+        var currentNode = openHeap.pop();
+  
+        // End case -- result has been found, return the traced path.
+        if (currentNode === end) {
+          return pathTo(currentNode);
+        }
+  
+        // Normal case -- move currentNode from open to closed, process each of its neighbors.
+        currentNode.closed = true;
+  
+        // Find all neighbors for the current node.
+        var neighbors = graph.neighbors(currentNode);
+  
+        for (var i = 0, il = neighbors.length; i < il; ++i) {
+          var neighbor = neighbors[i];
+  
+          if (neighbor.closed || neighbor.isWall()) {
+            // Not a valid node to process, skip to next neighbor.
+            continue;
+          }
+  
+          // The g score is the shortest distance from start to current node.
+          // We need to check if the path we have arrived at this neighbor is the shortest one we have seen yet.
+          var gScore = currentNode.g + neighbor.getCost(currentNode);
+          var beenVisited = neighbor.visited;
+  
+          if (!beenVisited || gScore < neighbor.g) {
+  
+            // Found an optimal (so far) path to this node.  Take score for node to see how good it is.
+            neighbor.visited = true;
+            neighbor.parent = currentNode;
+            neighbor.h = neighbor.h || heuristic(neighbor, end);
+            neighbor.g = gScore;
+            neighbor.f = neighbor.g + neighbor.h;
+            graph.markDirty(neighbor);
+            if (closest) {
+              // If the neighbour is closer than the current closestNode or if it's equally close but has
+              // a cheaper path than the current closest node then it becomes the closest node
+              if (neighbor.h < closestNode.h || (neighbor.h === closestNode.h && neighbor.g < closestNode.g)) {
+                closestNode = neighbor;
+              }
+            }
+  
+            if (!beenVisited) {
+              // Pushing to heap will put it in proper place based on the 'f' value.
+              openHeap.push(neighbor);
+            } else {
+              // Already seen the node, but since it has been rescored we need to reorder it in the heap
+              openHeap.rescoreElement(neighbor);
+            }
+          }
+        }
+      }
+  
+      if (closest) {
+        return pathTo(closestNode);
+      }
+  
+      // No result was found - empty array signifies failure to find path.
+      return [];
+    },
+    // See list of heuristics: http://theory.stanford.edu/~amitp/GameProgramming/Heuristics.html
+    heuristics: {
+      manhattan: function(pos0, pos1) {
+        var d1 = Math.abs(pos1.x - pos0.x);
+        var d2 = Math.abs(pos1.y - pos0.y);
+        return d1 + d2;
+      },
+      diagonal: function(pos0, pos1) {
+        var D = 1;
+        var D2 = Math.sqrt(2);
+        var d1 = Math.abs(pos1.x - pos0.x);
+        var d2 = Math.abs(pos1.y - pos0.y);
+        return (D * (d1 + d2)) + ((D2 - (2 * D)) * Math.min(d1, d2));
+      }
+    },
+    cleanNode: function(node) {
+      node.f = 0;
+      node.g = 0;
+      node.h = 0;
+      node.visited = false;
+      node.closed = false;
+      node.parent = null;
+    }
+  };
+  
+  /**
+   * A graph memory structure
+   * @param {Array} gridIn 2D array of input weights
+   * @param {Object} [options]
+   * @param {bool} [options.diagonal] Specifies whether diagonal moves are allowed
+   */
+  function Graph(gridIn, options) {
+    options = options || {};
+    this.nodes = [];
+    this.diagonal = !!options.diagonal;
+    this.grid = [];
+    for (var x = 0; x < gridIn.length; x++) {
+      this.grid[x] = [];
+  
+      for (var y = 0, row = gridIn[x]; y < row.length; y++) {
+        var node = new GridNode(x, y, row[y]);
+        this.grid[x][y] = node;
+        this.nodes.push(node);
+      }
+    }
+    this.init();
+  }
+  
+  Graph.prototype.init = function() {
+    this.dirtyNodes = [];
+    for (var i = 0; i < this.nodes.length; i++) {
+      astar.cleanNode(this.nodes[i]);
+    }
+  };
+  
+  Graph.prototype.cleanDirty = function() {
+    for (var i = 0; i < this.dirtyNodes.length; i++) {
+      astar.cleanNode(this.dirtyNodes[i]);
+    }
+    this.dirtyNodes = [];
+  };
+  
+  Graph.prototype.markDirty = function(node) {
+    this.dirtyNodes.push(node);
+  };
+  
+  Graph.prototype.neighbors = function(node) {
+    var ret = [];
+    var x = node.x;
+    var y = node.y;
+    var grid = this.grid;
+  
+    // West
+    if (grid[x - 1] && grid[x - 1][y]) {
+      ret.push(grid[x - 1][y]);
+    }
+  
+    // East
+    if (grid[x + 1] && grid[x + 1][y]) {
+      ret.push(grid[x + 1][y]);
+    }
+  
+    // South
+    if (grid[x] && grid[x][y - 1]) {
+      ret.push(grid[x][y - 1]);
+    }
+  
+    // North
+    if (grid[x] && grid[x][y + 1]) {
+      ret.push(grid[x][y + 1]);
+    }
+  
+    if (this.diagonal) {
+      // Southwest
+      if (grid[x - 1] && grid[x - 1][y - 1]) {
+        ret.push(grid[x - 1][y - 1]);
+      }
+  
+      // Southeast
+      if (grid[x + 1] && grid[x + 1][y - 1]) {
+        ret.push(grid[x + 1][y - 1]);
+      }
+  
+      // Northwest
+      if (grid[x - 1] && grid[x - 1][y + 1]) {
+        ret.push(grid[x - 1][y + 1]);
+      }
+  
+      // Northeast
+      if (grid[x + 1] && grid[x + 1][y + 1]) {
+        ret.push(grid[x + 1][y + 1]);
+      }
+    }
+  
+    return ret;
+  };
+  
+  Graph.prototype.toString = function() {
+    var graphString = [];
+    var nodes = this.grid;
+    for (var x = 0; x < nodes.length; x++) {
+      var rowDebug = [];
+      var row = nodes[x];
+      for (var y = 0; y < row.length; y++) {
+        rowDebug.push(row[y].weight);
+      }
+      graphString.push(rowDebug.join(" "));
+    }
+    return graphString.join("\n");
+  };
+  
+  function GridNode(x, y, weight) {
+    this.x = x;
+    this.y = y;
+    this.weight = weight;
+  }
+  
+  GridNode.prototype.toString = function() {
+    return "[" + this.x + " " + this.y + "]";
+  };
+  
+  GridNode.prototype.getCost = function(fromNeighbor) {
+    // Take diagonal weight into consideration.
+    if (fromNeighbor && fromNeighbor.x != this.x && fromNeighbor.y != this.y) {
+      return this.weight * 1.41421; // = sqr(2)
+    }
+    return this.weight;
+  };
+  
+  GridNode.prototype.isWall = function() {
+    return this.weight === 0;
+  };
+  
+  function BinaryHeap(scoreFunction) {
+    this.content = [];
+    this.scoreFunction = scoreFunction;
+  }
+  
+  BinaryHeap.prototype = {
+    push: function(element) {
+      // Add the new element to the end of the array.
+      this.content.push(element);
+  
+      // Allow it to sink down.
+      this.sinkDown(this.content.length - 1);
+    },
+    pop: function() {
+      // Store the first element so we can return it later.
+      var result = this.content[0];
+      // Get the element at the end of the array.
+      var end = this.content.pop();
+      // If there are any elements left, put the end element at the
+      // start, and let it bubble up.
+      if (this.content.length > 0) {
+        this.content[0] = end;
+        this.bubbleUp(0);
+      }
+      return result;
+    },
+    remove: function(node) {
+      var i = this.content.indexOf(node);
+  
+      // When it is found, the process seen in 'pop' is repeated
+      // to fill up the hole.
+      var end = this.content.pop();
+  
+      if (i !== this.content.length - 1) {
+        this.content[i] = end;
+  
+        if (this.scoreFunction(end) < this.scoreFunction(node)) {
+          this.sinkDown(i);
+        } else {
+          this.bubbleUp(i);
+        }
+      }
+    },
+    size: function() {
+      return this.content.length;
+    },
+    rescoreElement: function(node) {
+      this.sinkDown(this.content.indexOf(node));
+    },
+    sinkDown: function(n) {
+      // Fetch the element that has to be sunk.
+      var element = this.content[n];
+  
+      // When at 0, an element can not sink any further.
+      while (n > 0) {
+  
+        // Compute the parent element's index, and fetch it.
+        var parentN = ((n + 1) >> 1) - 1;
+        var parent = this.content[parentN];
+        // Swap the elements if the parent is greater.
+        if (this.scoreFunction(element) < this.scoreFunction(parent)) {
+          this.content[parentN] = element;
+          this.content[n] = parent;
+          // Update 'n' to continue at the new position.
+          n = parentN;
+        }
+        // Found a parent that is less, no need to sink any further.
+        else {
+          break;
+        }
+      }
+    },
+    bubbleUp: function(n) {
+      // Look up the target element and its score.
+      var length = this.content.length;
+      var element = this.content[n];
+      var elemScore = this.scoreFunction(element);
+  
+      while (true) {
+        // Compute the indices of the child elements.
+        var child2N = (n + 1) << 1;
+        var child1N = child2N - 1;
+        // This is used to store the new position of the element, if any.
+        var swap = null;
+        var child1Score;
+        // If the first child exists (is inside the array)...
+        if (child1N < length) {
+          // Look it up and compute its score.
+          var child1 = this.content[child1N];
+          child1Score = this.scoreFunction(child1);
+  
+          // If the score is less than our element's, we need to swap.
+          if (child1Score < elemScore) {
+            swap = child1N;
+          }
+        }
+  
+        // Do the same checks for the other child.
+        if (child2N < length) {
+          var child2 = this.content[child2N];
+          var child2Score = this.scoreFunction(child2);
+          if (child2Score < (swap === null ? elemScore : child1Score)) {
+            swap = child2N;
+          }
+        }
+  
+        // If the element needs to be moved, swap it, and continue.
+        if (swap !== null) {
+          this.content[n] = this.content[swap];
+          this.content[swap] = element;
+          n = swap;
+        }
+        // Otherwise, we are done.
+        else {
+          break;
+        }
+      }
+    }
+  };
+  
+  function findCost(matrix, startX, startY, endX, endY){
+    var graph = new Graph(matrix, { diagonal: true });
+    var start = graph.grid[startY][startX]
+    var end = graph.grid[endY][endX]
+    var result = astar.search(graph, start, end)
+    result.unshift(start);
+    //console.log(result)
+  
+    return result.length;
+  }
+
+//------------------------
+
+function entriesDist(entries, grid) {
+    for (let i = 0; i < entries.length; i++){
+
+        entries[i].costs = new Array();
+        n = 0;
+        for (let j = 0; j < entries.length; j++){
+            if (!(i==j)) {
+
+                console.log(entries[i].cords.y)
+                entries[i].costs[n] = {
+                    "id" : entries[j].id,
+                    "cost" : findCost(grid, entries[i].cords.x, entries[i].cords.y, entries[j].cords.x, entries[j].cords.y)
+                }
+                n++;
+            }
+        }
+    }
+}
+
 function ForemData(nodes) {
+    floorId = document.getElementById("fileId").value
     jsonData = {
         "name": document.getElementById("textInp").value,
+        "id": floorId,
         "res": {
             "width": imageEl.clientWidth,
             "height": imageEl.clientHeight
@@ -211,37 +643,44 @@ function ForemData(nodes) {
     for (let i = 0; i < rows; i++){
         grid[i] = new Array()
         for (let j =0; j < collums; j++){
-            if (nodes[i][j].walkable) {
+
+            if (nodes[j][i].walkable) {
                 grid[i][j] = 1;
             } else {
                 grid[i][j] = 0;
             }
 
-            if (nodes[i][j].type == "place") {
+            if (nodes[j][i].type == "place") {
                 places[placescount] = {
-                    "name": nodes[i][j].extra.place,
+                    "name": nodes[j][i].extra.place,
                     "cords": {
-                        "y": nodes[i][j].y,
-                        "x": nodes[i][j].x
+                        "y": nodes[j][i].y,
+                        "x": nodes[j][i].x
                     }
                 };
                 placescount++
             }
-
-            if (nodes[i][j].type == "entry") {
+            if (nodes[j][i].type == "elevator" || nodes[j][i].type == "stairs") {
+                console.log(j)
                 entries[entriescount] = {
-                    "entryTo": nodes[i][j].extra.entryTo,
-                    "num": nodes[i][j].extra.num,
+                    "code": nodes[j][i].extra.id + "_" + nodes[j][i].extra.level + "_" + floorId,
+                    "type": nodes[j][i].type,
+                    "id": nodes[j][i].extra.id,
+                    "level": nodes[j][i].extra.level,
                     "cords": {
-                        "y": nodes[i][j].y,
-                        "x": nodes[i][j].x
+                        "y": nodes[j][i].y,
+                        "x": nodes[j][i].x
                     }
                 }
+                console.log("df");
                 entriescount++
             }
         }
 
     }
+
+    entriesDist(entries, grid);
+
     jsonData.grid = grid;
     jsonData.places = places;
     jsonData.entries = entries;
@@ -261,4 +700,4 @@ function ForemData(nodes) {
     linkElement.setAttribute('href', dataUri);
     linkElement.setAttribute('download', exportFileDefaultName);
     linkElement.click();
- }
+}
